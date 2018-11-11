9d5451e1a6e4041284ff044c6b9b453d9f68e49d
Deepak Valechha <rs@gmail.com>
2018-11-11 14:03:56 -0500

    First Commit

---
+++ creational/singleton/singleton.go
@@ -0,0 +1,26 @@
+package singleton
+
+// Singleton interface
+type Counter interface {
+	AddOne() int
+}
+
+type counter struct {
+	count int
+}
+
+var instance *counter
+
+// GetInstance to return a Singleton
+func GetInstance() Counter {
+	if instance == nil {
+		//instance = &singleton{}
+		instance = new(counter)
+	}
+	return instance
+}
+
+func (s *counter) AddOne() int {
+	s.count++
+	return s.count
+}
